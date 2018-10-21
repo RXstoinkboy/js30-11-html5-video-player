e8d92ef3f9d6b00414c47cde64ca2a5c61e63609
RXstoinkboy <erykludwin@gmail.com>
2018-10-21 23:24:50 +0200

    just starting js

---
+++ script.js
@@ -0,0 +1,14 @@
+// get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.video');
+const progressBar = player.querySelector('.progress-bar');
+const progressField = player.querySelector('.progress-field');
+const playButton = player.querySelector('.play-button');
+const volumeSlider = player.querySelector('.volume-slider');
+const speedSlider = player.querySelector('.speed-slider');
+const skipBack = player.querySelector('.back');
+const skipForward = player.querySelector('.forward');
+
+// build functions
+
+// make event listeners
