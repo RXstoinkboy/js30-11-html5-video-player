8000725a79147705d14b07312328777873c808b0
RXstoinkboy <erykludwin@gmail.com>
2018-10-24 00:35:47 +0200

    finish styling + js. Next -> make fullscreen mode

--- script.js
+++ script.js
@@ -4,11 +4,67 @@ const video = player.querySelector('.video');
 const progressBar = player.querySelector('.progress-bar');
 const progressField = player.querySelector('.progress-field');
 const playButton = player.querySelector('.play-button');
-const volumeSlider = player.querySelector('.volume-slider');
-const speedSlider = player.querySelector('.speed-slider');
-const skipBack = player.querySelector('.back');
-const skipForward = player.querySelector('.forward');
+const sliders = player.querySelectorAll('input');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
 // build functions
 
+
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+}
+
+function updateButton() {
+    const icon = this.paused ? '▶' : '▮▮';
+    playButton.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function updateProperty() {
+    video[this.name] = this.value;
+}
+
+function updateTime() {
+    const percentage = (video.currentTime / video.duration) * 100;
+    progressField.style.flexBasis = `${percentage}%`;
+}
+
+function scrub(e) {
+    console.log(e);
+    const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 // make event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', updateTime);
+
+playButton.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+sliders.forEach(slider => slider.addEventListener('change', updateProperty));
+
+let mousedown = false;
+progressField.addEventListener('click', scrub);
+progressBar.addEventListener('click', scrub);
+progressField.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progressField.addEventListener('mousedown', () => mousedown = true);
+progressField.addEventListener('mouseup', () => mousedown = false);
+progressBar.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progressBar.addEventListener('mousedown', () => mousedown = true);
+progressBar.addEventListener('mouseup', () => mousedown = false);
+
+// TODO add fullscreen mode
